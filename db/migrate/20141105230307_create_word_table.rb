93906bdde6458482adfc4963d86aa4fe82bfcd93
karynntran <karynn.tran@gmail.com>
2014-11-10 09:34:57 -0500

    migrations deleted

--- db/migrate/20141105230307_create_word_table.rb
+++
@@ -1,7 +0,0 @@
-class CreateWordTable < ActiveRecord::Migration
-  def change
-    create_table :words do |t|
-      t.string :body
-    end
-  end
-end
--- db/migrate/20141109163131_add_avatar_photolink.rb
+++
@@ -1,5 +0,0 @@
-class AddAvatarPhotolink < ActiveRecord::Migration
-  def change
-    add_column :users, :avatar, :string
-  end
-end
